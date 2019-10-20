5e56e072c1d4f43f37442c4c9f4c437842bfa4d7
Peter Babič <peter@peterbabic.dev>
2019-10-20 13:57:49 +0200

    remove apollo-server from dependenices

--- src/server/UserResolver.spec.ts
+++ src/server/UserResolver.spec.ts
@@ -1,4 +1,4 @@
-import { gql } from "apollo-server"
+import { gql } from "apollo-server-express"
 import { Request, Response } from "express"
 import { createConnection, getConnection } from "typeorm"
 import { callSchema } from "./schema"
